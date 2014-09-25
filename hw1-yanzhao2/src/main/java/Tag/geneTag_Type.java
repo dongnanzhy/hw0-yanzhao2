df02d530408d7d9761f5db3edd6c4d5d08cfa324
Yan Zhao <dongnanzhy@gmail.com>
2014-09-24 20:20:21 -0700

    qq

---
+++ hw1-yanzhao2/src/main/java/Tag/geneTag_Type.java
@@ -0,0 +1,120 @@
+
+/* First created by JCasGen Tue Sep 23 07:31:01 PDT 2014 */
+package Tag;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** 
+ * Updated by JCasGen Tue Sep 23 07:31:01 PDT 2014
+ */
+public class geneTag_Type extends Annotation_Type {
+  /** 
+   * @return the generator for this type
+   */
+  @Override
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+  			 if (geneTag_Type.this.useExistingInstance) {
+  			   // Return eq fs instance if already created
+  		     FeatureStructure fs = geneTag_Type.this.jcas.getJfsFromCaddr(addr);
+  		     if (null == fs) {
+  		       fs = new geneTag(addr, geneTag_Type.this);
+  			   geneTag_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  			   return fs;
+  		     }
+  		     return fs;
+        } else return new geneTag(addr, geneTag_Type.this);
+  	  }
+    };
+
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = geneTag.typeIndexID;
+
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("Tag.geneTag");
+ 
+
+  final Feature casFeat_id;
+
+  final int     casFeatCode_id;
+  /** 
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getId(int addr) {
+        if (featOkTst && casFeat_id == null)
+      jcas.throwFeatMissing("id", "Tag.geneTag");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_id);
+  }
+  /** 
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setId(int addr, String v) {
+        if (featOkTst && casFeat_id == null)
+      jcas.throwFeatMissing("id", "Tag.geneTag");
+    ll_cas.ll_setStringValue(addr, casFeatCode_id, v);}
+    
+  
+ 
+
+  final Feature casFeat_content;
+
+  final int     casFeatCode_content;
+  /** 
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getContent(int addr) {
+        if (featOkTst && casFeat_content == null)
+      jcas.throwFeatMissing("content", "Tag.geneTag");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_content);
+  }
+  /** 
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setContent(int addr, String v) {
+        if (featOkTst && casFeat_content == null)
+      jcas.throwFeatMissing("content", "Tag.geneTag");
+    ll_cas.ll_setStringValue(addr, casFeatCode_content, v);}
+    
+  
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * 
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public geneTag_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_id = jcas.getRequiredFeatureDE(casType, "id", "uima.cas.String", featOkTst);
+    casFeatCode_id  = (null == casFeat_id) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_id).getCode();
+
+ 
+    casFeat_content = jcas.getRequiredFeatureDE(casType, "content", "uima.cas.String", featOkTst);
+    casFeatCode_content  = (null == casFeat_content) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_content).getCode();
+
+  }
+}
+
+
+
+    
